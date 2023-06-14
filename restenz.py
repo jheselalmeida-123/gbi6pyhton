c2a6ed7ba0ad22f256d52d554647cc222409354f
jheselalmeida-123 <131234680+jheselalmeida-123@users.noreply.github.com>
2023-06-14 18:23:08 -0500

    Update restenz.py

--- restenz.py
+++ restenz.py
@@ -1,2 +1,21 @@
-def ecor1(seq):
-    print("hola")
+seq ="CGATGATGAATTCGTACCCGAGCTGAATTCAGCAGAATTCAGCTGATCGATACCAGAATTCCATA"
+ecoRI = "GAATTC"
+def ecor1(enzima, seq):
+    sitios_corte = []
+    seq_copy = seq  # Copia de la secuencia original
+    
+    while True:
+        sitiocorte = seq_copy.find(enzima)
+        if sitiocorte == -1:
+            break
+        frag1 = seq_copy[:sitiocorte + len(enzima)]
+        frag2 = seq_copy[sitiocorte + len(enzima):]
+        sitios_corte.append(frag1)
+        seq_copy = frag2
+    
+    sitios_corte.append(seq_copy)  # Agregar el último fragmento restante
+    
+    return sitios_corte
+
+resultados = ecor1(ecoRI, seq)
+print(resultados)
