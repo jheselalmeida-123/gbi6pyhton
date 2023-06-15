31aa2b6c2699f4cbac8ae03ecc68aef8aa768113
jheselalmeida-123 <131234680+jheselalmeida-123@users.noreply.github.com>
2023-06-14 23:18:41 -0500

    Update restenz.py

--- restenz.py
+++ restenz.py
@@ -1,3 +1,37 @@
+def traductor(secuencia): 
+  """
+  Función traductor realiza la traducción de nucleótidos a aminoácidos
+  input: 
+  secuencia: secuencia de nucleótidos
+  output: 
+  sencuecia: secuencia de nucleótidos codificantes
+  péptido: secuencia de amino ácidos
+  """
+  seqc = secuencia[secuencia.find("ATG"):]
+  peptido = ""
+  for i in range(len(seqc)):
+      i *= 3
+      codon = seqc[i:i+3]
+      if len(codon) < 3: # no reconoce como codon si hay 0,1,2 Nucleotidos
+          break
+      AmAc = codon_dictionary[codon] # cambiar por un código
+      if AmAc =="_":
+        break
+      peptido += AmAc #equivale al append
+  return([seqc,peptido])
+
+
+def gcp(secuencia):
+   """
+   gcp realiza el cálculo del porcentaje de contenido de Gc y Cs
+   input: secuencia de nucléotidos
+   output: porcentaje de gc
+   """
+   pgc = round(100*(secuencia.count("C") + secuencia.count("G"))/len(secuencia), 2)
+   return pgc
+
+
+
 # Empalme de las enzimas EcoR1
 
 seq = "CGATGATGAATTCGTACCCGAGCTGAATTCAGCAGAATTCAGCTGATCGATACCAGAATTCCATA"
