45ff12742f49c8994cf0ee8e5497a01f49b00011
jheselalmeida-123 <131234680+jheselalmeida-123@users.noreply.github.com>
2023-06-14 22:52:47 -0500

    Update restenz.py

--- restenz.py
+++ restenz.py
@@ -1,9 +1,11 @@
-seq ="CGATGATGAATTCGTACCCGAGCTGAATTCAGCAGAATTCAGCTGATCGATACCAGAATTCCATA"
+# Empalme de las enzimas EcoR1
+
+seq = "CGATGATGAATTCGTACCCGAGCTGAATTCAGCAGAATTCAGCTGATCGATACCAGAATTCCATA"
 ecoRI = "GAATTC"
 def ecor1(enzima, seq):
     sitios_corte = []
     seq_copy = seq  
-    
+
     while True:
         sitiocorte = seq_copy.find(enzima)
         if sitiocorte == -1:
@@ -12,10 +14,59 @@ def ecor1(enzima, seq):
         frag2 = seq_copy[sitiocorte + len(enzima):]
         sitios_corte.append(frag1)
         seq_copy = frag2
-    
+
     sitios_corte.append(seq_copy)  
-    
+
     return sitios_corte
 
 resultados = ecor1(ecoRI, seq)
+print(resultados)
+
+#Empalme de las enzimas HindIII
+
+seq = "CGATGATGAATTCGTACCCGAGCTGAATTCAGCAGAATTCAGCTGATCGATACCAGAATTCCATA"
+HindIII = "AAGCTT"
+def HindIII1(enzima, seq):
+    sitios_corte = []
+    seq_copy = seq  
+
+    while True:
+        sitiocorte = seq_copy.find(enzima)
+        if sitiocorte == -1:
+            break
+        frag1 = seq_copy[:sitiocorte + len(enzima)]
+        frag2 = seq_copy[sitiocorte + len(enzima):]
+        sitios_corte.append(frag1)
+        seq_copy = frag2
+
+    sitios_corte.append(seq_copy)  
+
+    return sitios_corte
+
+resultados = HindIII1 (HindIII, seq)
+print(resultados)
+
+
+#Empalme de las enzimas NotI
+
+seq = "CGATGATGAATTCGTACCCGAGCTGAATTCAGCAGAATTCAGCTGATCGATACCAGAATTCCATA"
+NotI = "GCGGCCGC"
+def NotI1 (enzima, seq):
+    sitios_corte = []
+    seq_copy = seq  
+
+    while True:
+        sitiocorte = seq_copy.find(enzima)
+        if sitiocorte == -1:
+            break
+        frag1 = seq_copy[:sitiocorte + len(enzima)]
+        frag2 = seq_copy[sitiocorte + len(enzima):]
+        sitios_corte.append(frag1)
+        seq_copy = frag2
+
+    sitios_corte.append(seq_copy)  
+
+    return sitios_corte
+
+resultados = NotI1 (NotI, seq)
 print(resultados)
